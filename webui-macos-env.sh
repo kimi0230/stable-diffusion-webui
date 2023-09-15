3b96dbf9f397e6c5827e2d99d054341b5a31abe9
kimiimac <kimi0230@gmail.com>
2023-09-15 22:28:50 +0800

    chore: mac

--- webui-macos-env.sh
+++ webui-macos-env.sh
@@ -10,7 +10,8 @@ then
 fi
 
 export install_dir="$HOME"
-export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate"
+# export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate"
+export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --xformers"
 export TORCH_COMMAND="pip install torch==2.0.1 torchvision==0.15.2"
 export K_DIFFUSION_REPO="https://github.com/brkirch/k-diffusion.git"
 export K_DIFFUSION_COMMIT_HASH="51c9778f269cedb55a4d88c79c0246d35bdadb71"
