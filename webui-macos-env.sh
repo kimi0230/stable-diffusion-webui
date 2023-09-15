4615e1fdb2bce3593548faaca2963862f3786e12
kimiimac <kimi0230@gmail.com>
2023-09-15 22:31:47 +0800

    chore: mac

--- webui-macos-env.sh
+++ webui-macos-env.sh
@@ -11,7 +11,7 @@ fi
 
 export install_dir="$HOME"
 # export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate"
-export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --xformers"
+export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --xformers --medvram"
 export TORCH_COMMAND="pip install torch==2.0.1 torchvision==0.15.2"
 export PYTORCH_ENABLE_MPS_FALLBACK=1
 
